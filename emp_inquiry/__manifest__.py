d5b0a60fa86190eea9ed6aef3a56ba5caa2631a8
adarsh-patel <pateladarsh14@gmail.com>
2018-06-05 15:53:00 +0530

    [ADD] module structure

---
+++ emp_inquiry/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name':'emp_inquiry',
+    'description':'Employee Inquiry Form.',
+    'version':'1.0',
+    'category':'website-apps',
+    'author':'Bista solutions',
+    'depends':['base','website'],
+    'data':[
+
+    ],
+
+    'installable':True,
+    'auto-install':False,
+    'application':False,
+}
