1f92033abf5b659c7adf94fdfc96a626eb123c9a
adarsh-patel <pateladarsh14@gmail.com>
2018-06-06 11:04:03 +0530

    [ADD] add form data in database

--- emp_inquiry/controllers/main.py
+++ emp_inquiry/controllers/main.py
@@ -1,9 +1,39 @@
 from odoo import http
 from odoo.http import request
+from odoo.addons.website.controllers.main import QueryURL
+
 
 class inquiry_from(http.Controller):
 
     @http.route('/emp/inquiry',type='http',auth='public',website='True')
-    def emp_form(self):
+    def emp_form(self, **kw):
         print('\n\n\n Hello Website')
         return request.render('emp_inquiry.emp_form')
+
+    @http.route('/emp/create_inquiry',type='http',auth='public',website='True')
+    def create_inquiry(self, **post):
+        cutomer_no = str(post.get('cutomer_no'))
+        first_name = str(post.get('first_name'))
+        last_name = str(post.get('last_name'))
+        street = str(post.get('street'))
+        city = str(post.get('city'))
+        country = str(post.get('country'))
+        dob = str(post.get('dob'))
+        proof_id = str(post.get('proof_id'))
+        security_no = str(post.get('security_no'))
+        employer_name = str(post.get('employer_name'))
+        employer_income = str(post.get('employer_income'))
+        account_no = str(post.get('account_no'))
+        ifsc_code = str(post.get('ifsc_code'))
+        notes = str(post.get('notes'))
+
+        vals = {'cutomer_no':cutomer_no,'first_name':first_name,'last_name':last_name,
+                'street':street,'city':city,'country':country,'dob':dob,'proof_id':proof_id,
+                'security_no':security_no,'employer_name':employer_name,'employer_income':employer_income,
+                'account_no':account_no,'ifsc_code':ifsc_code,'notes':notes}
+
+        new_rec = request.env['emp.inquiry'].create(vals)
+
+        print ('\n\n\n new_rec----------------------->',new_rec)
+
+        return ('THANKS FOR INQUIRY.')
