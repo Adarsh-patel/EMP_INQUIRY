42638051b09afe4363f54cc1abbcec375c51556d
adarsh-patel <pateladarsh14@gmail.com>
2018-06-07 18:20:58 +0530

    [ADD] changes in js

--- emp_inquiry/static/src/js/form_validation.js
+++ emp_inquiry/static/src/js/form_validation.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-    $(input).focus(function(){
-        $(this).css('border-radius','50px')
+odoo.define('emp_inquiry.Form_validation',function(){
+
+    $(document).ready(function(){
+        $("input").focus(function(){
+            $(this).css('border','2px solid red')
+        });
     });
 });
