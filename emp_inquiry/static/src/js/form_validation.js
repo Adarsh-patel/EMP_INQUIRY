5a9180e45eb5c0b9d24829c75773b97f1138aeae
adarsh-patel <pateladarsh14@gmail.com>
2018-06-08 13:02:50 +0530

    [ADD] changes in js

--- emp_inquiry/static/src/js/form_validation.js
+++ emp_inquiry/static/src/js/form_validation.js
@@ -1,7 +1,7 @@
 odoo.define('emp_inquiry.Form_validation',function(){
 
     $(document).ready(function(){
-        $("input").focus(function(){
+        $("input").change(function(){
             $(this).css('border','2px solid red')
         });
     });
